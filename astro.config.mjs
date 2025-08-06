dea5991a2c5cd9f39ade82bf2132bd30979195f4
Skylar <118846397+NobleSkye@users.noreply.github.com>
2025-08-06 04:06:46 -0700

    Add Clerk integration to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import clerk from '@clerk/astro';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    clerk({
+      afterSignInUrl: '/dashboard',
+      afterSignUpUrl: '/dashboard',
+      signInUrl: '/login',
+      signUpUrl: '/signup'
+    })
+  ],
   site: 'https://blahaj.bio',
   base: '/',
-  output: 'static',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   build: {
     format: 'file'
   }
