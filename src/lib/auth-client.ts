f214d03f34dd6b6e4e4419d1b9a88e5a29452ec6
Skye <118846397+NobleSkye@users.noreply.github.com>
2025-09-10 19:00:16 +0000

    init

--- src/lib/auth-client.ts
+++
@@ -1,7 +0,0 @@
-import { createAuthClient } from "better-auth/client";
-
-export const authClient = createAuthClient({
-  // baseURL optional when same origin
-});
-
-export const { signIn, signOut, getSession, useSession } = authClient;
--- src/pages/api/auth/[...all].ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from "astro";
-import { auth } from "../../../lib/auth";
-
-// Catch-all handler to mount Better Auth at /api/auth/*
-export const ALL: APIRoute = async ({ request }) => {
-  const res = await auth.handler(request);
-  return res as Response;
-};
